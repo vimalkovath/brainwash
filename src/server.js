6d2e70e96ae25fedb716381f15399722f399a887
admin <vimal.k@fulfillsolutions.com>
2019-02-07 19:58:47 +0530

    add

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/your_app_name'));
+app.use(express.static('./dist/wework-quiz-application'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+res.sendFile(path.join(__dirname,'/dist/wework-quiz-application/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
